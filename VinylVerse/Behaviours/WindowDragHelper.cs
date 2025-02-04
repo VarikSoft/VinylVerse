c4e34f2bac5c029c67a463336b0d241034d0d585
Anton Liubetskyi <melllipton@gmail.com>
2025-02-04 16:37:42 +0100

    Start working on sign up menu

---
+++ VinylVerse/Behaviours/WindowDragHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace VinylVerse.Behaviours
+{
+    internal class WindowDragHelper
+    {
+        public static void EnableDrag(Window window, UIElement draggableElement)
+        {
+            draggableElement.MouseDown += (sender, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    window.DragMove();
+                }
+            };
+        }
+    }
+}
